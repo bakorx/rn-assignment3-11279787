32fe0322e64b8e0572d1dec702efc9441c2da546
bakorx <ekbaddai@st.ug.edu.gh>
2024-06-01 19:10:01 +0000

    header

--- nativeapp/App.js
+++ nativeapp/App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+      <View style={styles.headerRow}>
+  <View>
+    <Text style={styles.headerTitle}>Hello, Devs</Text>
+    <Text>14 tasks today</Text>
+  </View>
+  <Image
+    source={require("./assets/profile.jpg")}
+    style={styles.profileImage}
+  />
+  <StatusBar style="auto" />
+</View>
+
+      
+      </View>
     </View>
   );
 }
@@ -13,8 +26,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8D1BA',
+  },
+
+  content: {
+    paddingHorizontal: 20,
+    paddingTop: 40,
+  },
+  headerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    fontSize: 32,
+    fontWeight: "bold",
   },
 });
